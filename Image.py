7c278933bdbb44c77a8b5babb0de79379d19c4a2
darthpedroo <pedrovillarino06@gmail.com>
2024-01-14 02:32:12 -0300

    Increased confidence

--- Image.py
+++ Image.py
@@ -2,8 +2,7 @@ import pyautogui
 class Image():
     def __init__(self, image):
         self.image = image
-        self.confidence = 0.6
+        self.confidence = 0.7
     
     def get_image_pos(self):
-        
         return pyautogui.locateOnScreen(self.image, grayscale=True, confidence=self.confidence)

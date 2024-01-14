e267b9e1509b1175ef9d58d90a3a8c03770e0a9b
darthpedroo <pedrovillarino06@gmail.com>
2024-01-14 02:32:39 -0300

    Separated the code into a test

--- main.py
+++ main.py
@@ -4,19 +4,5 @@ import keyboard
 import random
 from test import *
 
-royal_giant_image = "assets/RoyalGiant.jpg"
-imageLocation =  pyautogui.locateOnScreen(royal_giant_image, grayscale=True, confidence=.8)
+test_arena()
 
-print(imageLocation)
-print(type(imageLocation))
-print(imageLocation.left)
-
-#pyautogui.displayMousePosition()
-
-pyautogui.click(imageLocation.left + 10, imageLocation.top)
-pyautogui.click(random.randint(1074,1143), random.randint(130,480))
-
-
-
-# X = 1074 - 1143
-# Y = 480 - 130
--- test.py
+++ test.py
@@ -1,33 +1,35 @@
 from Card import *
+from Deck import *
 import time
-import json
-path = "assets/cards/"
-
-
-cards_json = "Deck.json"
-with open(cards_json, 'r') as file:
-    data = json.load(file)
-
-main_card_url = (data["farming_deck_1_urls"][0]["main_card"])
-cycle_cards_url = (data["farming_deck_1_urls"][1]["cycle_cards"])
-
-main_card_path = path + main_card_url
-main_card = Card(main_card_path)
-
-cycle_cards_paths = []
-cycle_cards = [] #Instances
-
-for card in cycle_cards_url:
-    current_path = path + card
-    card_instance = Card(current_path)
-    cycle_cards_paths.append(current_path)
-    cycle_cards.append(card_instance)
 
 
 
-for card in cycle_cards:
-    print("CARTA: ", card.image)
-    card.click_card()
-    time.sleep(1)
+def test_arena():
 
-main_card.click_card()
+    deck = Deck()
+
+    main_card = Card(deck.get_main_card_path())
+    cycle_cards = []
+
+    for card_path in deck.get_cycle_cards_path():
+        card_instance = Card(card_path)
+        cycle_cards.append(card_instance)
+
+
+    print("CYcle ", cycle_cards)
+
+    while True:
+        if main_card.get_image_pos() is not None: #Si la carta está en la barra de cartas
+            main_card.deploy_card_at_bridge()
+            time.sleep(3)
+        else:
+            for card in cycle_cards:
+                print("CARTA: ", card.image)
+                print(card.get_image_pos())
+                if card.get_image_pos() is not None:
+                    print("CARTA NO NULA: ", card.image)
+                    card.deploy_card_at_bridge()
+                    time.sleep(3)
+                    break
+
+            

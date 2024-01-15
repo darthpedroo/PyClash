8c736bfc8fb25984fe300116d9e952a47f941f70
darthpedroo <pedrovillarino06@gmail.com>
2024-01-15 15:21:53 -0300

    testing MainCard and CycleCard

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from Gui import Gui 
-import time
+from Deck import Deck
+from MainCard import MainCard
+from CycleCard import CycleCard
 
-gui = Gui()
+cards = []
+main_card = MainCard()
 
-time.sleep(2)
-gui.play_again()
+cards.append(main_card)
+
+for i in range (0,7):
+    cycle_card = CycleCard(i)
+    cards.append(cycle_card)
+
+for i in cards:
+    print(i.get_card_path())
+    print(i.is_main_card())
